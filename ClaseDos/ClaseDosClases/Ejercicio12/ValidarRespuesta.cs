e23499c9b9c7012d46695a5046f6a5547a8f61e3
Zinad <leandrosineriz@gmail.com>
2017-08-23 07:06:16 -0300

    ClaseDos

---
+++ ClaseDos/ClaseDosClases/Ejercicio12/ValidarRespuesta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12
+{
+    class ValidarRespuesta
+    {
+        public static bool ValidaS_N()
+        {
+            string respuesta;
+            bool retorno = false;
+            Console.WriteLine("Ingrese S para continuar o N para salir");
+
+            respuesta=Console.ReadLine();
+            respuesta = respuesta.ToLower();
+
+            while (respuesta != "n" && respuesta != "s")
+            {
+                Console.WriteLine("Error. reingrese S para seguir o N para salir");
+                respuesta = Console.ReadLine();
+                respuesta = respuesta.ToLower();
+            }
+
+            if (respuesta == "s")
+            {
+                retorno = true;
+
+            }
+
+            return retorno;
+
+
+
+        }
+
+    }
+}
---
+++ ClaseDos/ClaseDosClases/primerEjercicio/Clase1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primerEjercicio
+{
+    class Persona
+    {
+        public static void Saludar()
+        {
+            Console.WriteLine("Hola");
+        }
+
+        public void SaludarDos()
+        {
+            Console.WriteLine("Hola dos");
+        }
+    }
+}
