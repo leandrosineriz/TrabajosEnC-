6a6820f91f17fe2fdb6af0c7ba3f207f710bf59d
Zinad <leandrosineriz@gmail.com>
2017-08-30 07:00:17 -0300

    ClaseCuatro
    Sobrecarga de metodos

---
+++ Trabajos Clase/ClaseCuatro/ClaseCuatro/Cosa.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseCuatro
+{
+    class Cosa
+    {
+        public int entero;
+        public string cadena;
+        public DateTime fecha;
+
+        /// <summary>
+        /// Constructor que inzializa en 0, "Sin Valor", DateTime.Now
+        /// </summary>
+        public Cosa()
+        {
+            this.entero = 0;
+            this.cadena = "Sin Valor";
+            this.fecha = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor que inzializa en num, "Sin Valor", DateTime.Now
+        /// </summary>
+        /// <param name="num">un numero</param>
+        public Cosa(int x):this()//como llamar cadenas desde cadena
+        {          
+            this.entero = x;
+            //se Repite Codigo Vamos A Optimizarlo
+            /*this.cadena = "Sin Valor";
+            this.fecha = DateTime.Now;*/
+        }
+
+        /// <summary>
+        /// Constructor que inzializa en num, cadena, DateTime.Now
+        /// </summary>
+        /// <param name="num">un numero</param>
+        /// <param name="cadena">un string</param>
+        public Cosa(int num,string cadena):this(num)//tiene un orden llama al constructor con parametro num
+        {
+            this.cadena = cadena;
+            //this.fecha = DateTime.Now;
+
+        }
+
+        public Cosa(DateTime fecha, int num, string cadena):this(num,cadena)
+        {
+            this.fecha = fecha;
+ 
+        }
+
+
+
+
+ /// <summary>
+ /// Establece el valor de entero
+ /// </summary>
+ /// <param name="num">un Numero</param>
+        public void EstablecerValor(int num)
+        {
+            this.entero=num;
+        }
+        /// <summary>
+        /// establece el valor de Cadena
+        /// </summary>
+        /// <param name="cadena">Una cadena</param>
+        public void EstablecerValor(string cadena)
+        {
+            this.cadena = cadena;
+        }
+        /// <summary>
+        /// Establece el valor de Fecha
+        /// </summary>
+        /// <param name="fecha">Una fecha</param>
+        public void EstablecerValor(DateTime fecha)
+        {
+            this.fecha = fecha;
+        }
+        /// <summary>
+        /// Muestra todos los datos por consola
+        /// </summary>
+        /// <returns>Concatenacion de todos los datos de la clase Cosa</returns>
+        public string Mostrar()
+        {
+            
+            return this.entero + " " + this.cadena + " " + this.fecha;
+        }
+
+        public string Mostrar(ConsoleColor color)
+        {
+            
+            Console.ForegroundColor = color;
+            return this.entero + " " + this.cadena + " " + this.fecha;
+        }
+
+      }
+}
---
+++ Trabajos Clase/ClaseCuatro/ClaseCuatro/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseCuatro
+{
+    class Program
+    {
+        enum eCosa { cosaUno=25, cosaDos , cosaTres }
+
+        static void Main(string[] args)
+        {
+            Cosa cosita = new Cosa(DateTime.UtcNow,8,"Cosita");
+            ConsoleColor color = ConsoleColor.Cyan;
+            //DateTime fechaAhora=;
+            
+            /*cosita.EstablecerValor(1);
+            cosita.EstablecerValor("Hola");
+            //con el constructor se pueden pasar los valores de una fecha en int
+            cosita.EstablecerValor(new DateTime(2017,08,29,09,51,10));
+            */
+
+            Random numeroRandom=new Random();
+            int num;
+            int contador=0;
+            int contadorRojo = 0;
+            int contadorVerde = 0;
+            int contadorMagenta = 0;
+
+            
+            while (contador < 100)
+            {
+                num=numeroRandom.Next(1, 70);
+                contador++;
+            switch (num)
+            {
+                    
+                case 1: color = ConsoleColor.Green;
+                    contadorVerde++;
+                    break;
+                case 2:
+                    color = ConsoleColor.Magenta;
+                    contadorMagenta++;
+                        break;
+                case 3:
+                    color=ConsoleColor.Red;
+                    contadorRojo++;
+                        break;
+                
+                default:
+                        color = ConsoleColor.Red;
+                        contadorRojo++;
+                    break;
+            }
+                Console.WriteLine(cosita.Mostrar(color));
+ 
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Rojos:" + contadorRojo + "Verdes:" + contadorVerde + "Magenta:" + contadorMagenta);
+
+            Console.Read();
+        }
+    }
+}
